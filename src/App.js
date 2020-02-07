e09b7fadbf8e40726673f3d23bd47c0b0b40165a
AliyaGII <alya.stark.i.i@gmail.com>
2020-02-07 11:07:38 +0500

    fixed #1, fixed #2, fixed #3, fixed #4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import Tele from './Tele/Tele'
+import Tariff from './Tariff/Tariff'
 
 export default function App(){
     const arr = [
-        {name: 'Мой онлайн+', price: 700, limit:"безлимит на Tele2 в России", min: 800, gb: 30 , button: 'Купить SIM' },
-        {name: 'Везде онлайн', price: 500, limit:"безлимит на Tele2 в России", min: 500, gb: 40, button: 'Купить SIM'},
-        {name: 'Мой онлайн', price: 400, limit:"безлимит на Tele2 в России", min: 500, gb: 15, button: 'Купить SIM'},
+        {name: 'Мой онлайн+', price: 700, limit:"безлимит на Tariff2 в России", min: 800, gb: 30 , button: 'Купить SIM' },
+        {name: 'Везде онлайн', price: 500, limit:"безлимит на Tariff2 в России", min: 500, gb: 40, button: 'Купить SIM'},
+        {name: 'Мой онлайн', price: 400, limit:"безлимит на Tariff2 в России", min: 500, gb: 15, button: 'Купить SIM'},
     ];
     return(
         <div>
-        <Tele name={arr[0].name} price={arr[0].price} limit={arr[0].limit} min={arr[0].min} gb={arr[0].gb} button={arr[0].button} />
-        <Tele name={arr[1].name} price={arr[1].price} limit={arr[1].limit} min={arr[1].min} gb={arr[1].gb} button={arr[1].button} />
-        <Tele name={arr[2].name} price={arr[2].price} limit={arr[2].limit} min={arr[2].min} gb={arr[2].gb} button={arr[2].button} />
+        <Tariff name={arr[0].name} price={arr[0].price} gb={arr[0].gb} />
+        <Tariff name={arr[1].name} price={arr[1].price} gb={arr[1].gb} />
+        <Tariff name={arr[2].name} price={arr[2].price} gb={arr[2].gb} />
         </div>
     )
 }
